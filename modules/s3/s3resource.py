0891f2844ebbc9e285f881fe40920d29aad0ff9d
Dominic König <dominic@nursix.org>
2017-12-21 13:04:43 +0100

    Backport bugfix from trunk

--- modules/s3/s3resource.py
+++ modules/s3/s3resource.py
@@ -6064,7 +6064,7 @@ class S3ResourceData(object):
             # Don't penalize the smallest filter results (=effective filtering)
             if limit:
                 maxids = max(limit, 200)
-                limitby_ = (start, maxids)
+                limitby_ = (start, start + maxids)
             else:
                 limitby_ = None
 
